ea8f68f0cc4ae8f170a031464989761425e5ebd8
kamva-hanisi <lucashanisi@gmail.com>
2025-03-07 15:05:09 +0200

    add activities

--- App.js
+++ App.js
@@ -11,3 +11,46 @@ const showMenu=()=>{
     sidebar.classList.toggle("active")
 
 }
+
+TweenMax.from('.navbar',1,{
+    delay:.3,
+    x:-40,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.header-headline',2,{
+    delay:.5,
+    y:80,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.header-para',3,{
+    delay:.5,
+    y:20,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.btn',4,{
+    delay:.5,
+    y:20,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('form',5,{
+    delay:0.3,
+    y:80,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.property-info',6,{
+    delay:0.5,
+    x:-100,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
+TweenMax.from('.property-card',7,{
+    delay:0.5,
+    y:200,
+    opacity:0,
+    ease:Expo.easeInOut
+    })
